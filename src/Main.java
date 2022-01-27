db8294b8f4b83d2abe0402d53a77ebc634f2dec6
y.tavadyan <yuratavadyan@gmail.com>
2022-01-27 20:45:06 +0300

    HomeWorkVariables Finished

--- src/Main.java
+++ src/Main.java
@@ -14,84 +14,84 @@ public class Main {
 
         //Task 2//
 
-    float Boxer1 = 78.2f;
-    float Boxer2 = 82.7f;
+    float firstBoxer = 78.2f;
+    float secondBoxer = 82.7f;
 
-    float TotalWeight = Boxer1 + Boxer2;
-        System.out.println("Общий вес боксеров = " + TotalWeight);
+    float totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес боксеров = " + totalWeight);
 
-    double WeightDifference = Boxer2 - Boxer1;
-        System.out.println("Разница в весе = " + WeightDifference);
+    double weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Разница в весе = " + weightDifference);
 
 
         //Task 3//
 
-    int Bananas = 5;
-    int BananaWeight =80;
-    int TotalBanansWeight = BananaWeight * Bananas;
+    int bananas = 5;
+    int bananaWeight =80;
+    int totalBanansWeight = bananaWeight * bananas;
 
-    int MilkVolume = 200;
-    int MilkWeight = (MilkVolume / 100) * 105;
+    int milkVolume = 200;
+    int milkWeight = (milkVolume / 100) * 105;
 
-    int Icecream = 2;
-    int IcecreamWeight = 100;
-    int TotalIcecreamWeight = IcecreamWeight * Icecream;
+    int icecream = 2;
+    int icecreamWeight = 100;
+    int totalIcecreamWeight = icecreamWeight * icecream;
 
 
-    int Eggs = 4;
-    int EggWeight = 70;
-    int TotalEggsWeight = Eggs * EggWeight;
+    int eggs = 4;
+    int eggWeight = 70;
+    int totalEggsWeight = eggs * eggWeight;
 
     //BF - Breakfast//
-    int TotalBFWeight = TotalBanansWeight + TotalIcecreamWeight + TotalEggsWeight + MilkWeight;
-        System.out.println("Вес такого спорт-завтрака = " + TotalBFWeight + " г.");
+    int totalBFWeight = totalBanansWeight + totalIcecreamWeight + totalEggsWeight + milkWeight;
+        System.out.println("Вес такого спорт-завтрака = " + totalBFWeight + " г.");
 
-    double TotalBFWeightInKg = TotalBFWeight / 1000;
-        System.out.println("Вес такого спорт-завтрака в кг = " + TotalBFWeightInKg + " кг.");
+    double totalBFWeightInKg = totalBFWeight / 1000;
+        System.out.println("Вес такого спорт-завтрака в кг = " + totalBFWeightInKg + " кг.");
 
 
 
         //Task 4//
 
-    double WeightLossAim = 7;
-    double SlowWeightLoss = 0.25;
-    double FastWeightLoss = 0.5;
+    double weightLossAim = 7;
+    double slowWeightLoss = 0.25;
+    double fastWeightLoss = 0.5;
 
-    double DaysSlowLoss = WeightLossAim / SlowWeightLoss;
-        System.out.println("Кол-во дней для достижения нужного рез-та при потере 250гр. в день = " + DaysSlowLoss);
-    double DaysFastLoss = WeightLossAim / FastWeightLoss;
-        System.out.println("Кол-во дней для достижения нужного рез-та при потере 500гр. в день =" + DaysFastLoss);
-    double DaysMid = (DaysFastLoss + DaysSlowLoss) / 2;
-        System.out.println("В среднем потребуется " + DaysMid + " дней, что бы достигнуть нужного веса");
+    double daysSlowLoss = weightLossAim / slowWeightLoss;
+        System.out.println("Кол-во дней для достижения нужного рез-та при потере 250гр. в день = " + daysSlowLoss);
+    double daysFastLoss = weightLossAim / fastWeightLoss;
+        System.out.println("Кол-во дней для достижения нужного рез-та при потере 500гр. в день =" + daysFastLoss);
+    double daysMid = (daysFastLoss + daysSlowLoss) / 2;
+        System.out.println("В среднем потребуется " + daysMid + " дней, что бы достигнуть нужного веса");
 
 
 
         //Task 5//
 
-    int MashaSalary = 67760;
-    int DenisSalary = 83690;
-    int KristinaSalary = 76230;
-    float MashaIncreasedSalary = (float) (MashaSalary * 1.1);
-    float DenisIncreasedSalary = (float) (DenisSalary * 1.1);
-    float KristinaIncreasedSalary = (float) (KristinaSalary * 1.1);
+    int mashaSalary = 67760;
+    int denisSalary = 83690;
+    int kristinaSalary = 76230;
+    float mashaIncreasedSalary = (float) (mashaSalary * 1.1);
+    float denisIncreasedSalary = (float) (denisSalary * 1.1);
+    float kristinaIncreasedSalary = (float) (kristinaSalary * 1.1);
 
-    int MashaYearIncome = MashaSalary * 12;
-    float MashaIncreasedYearIncome = MashaIncreasedSalary * 12;
-    float MashaIncomeDifference = MashaIncreasedYearIncome - MashaYearIncome;
+    int mashaYearIncome = mashaSalary * 12;
+    float mashaIncreasedYearIncome = mashaIncreasedSalary * 12;
+    float mashaIncomeDifference = mashaIncreasedYearIncome - mashaYearIncome;
 
-    int DenisYearIncome = DenisSalary * 12;
-    float DenisIncreasedYearIncome = DenisIncreasedSalary * 12;
-    float DenisIncomeDifference = DenisIncreasedYearIncome - DenisYearIncome;
+    int denisYearIncome = denisSalary * 12;
+    float denisIncreasedYearIncome = denisIncreasedSalary * 12;
+    float denisIncomeDifference = denisIncreasedYearIncome - denisYearIncome;
 
-    int KristinaYearIncome = KristinaSalary * 12;
-    float KristinaIncreasedYearIncome = KristinaIncreasedSalary * 12;
-    float KristinaIncomeDifference = KristinaIncreasedYearIncome - KristinaYearIncome;
+    int kristinaYearIncome = kristinaSalary * 12;
+    float kristinaIncreasedYearIncome = kristinaIncreasedSalary * 12;
+    float kristinaIncomeDifference = kristinaIncreasedYearIncome - kristinaYearIncome;
 
-        System.out.println("Маша теперь получет " + MashaIncreasedSalary + " рублей. Годовой доход вырос на " + MashaIncomeDifference + " рублей.");
+        System.out.println("Маша теперь получет " + mashaIncreasedSalary + " рублей. Годовой доход вырос на " + mashaIncomeDifference + " рублей.");
 
-        System.out.println("Денис теперь получет " + DenisIncreasedSalary + " рублей. Годовой доход вырос на " + DenisIncomeDifference + " рублей.");
+        System.out.println("Денис теперь получет " + denisIncreasedSalary + " рублей. Годовой доход вырос на " + denisIncomeDifference + " рублей.");
 
-        System.out.println("Кристина теперь получет " + KristinaIncreasedSalary + " рублей. Годовой доход вырос на " + KristinaIncomeDifference + " рублей.");
+        System.out.println("Кристина теперь получет " + kristinaIncreasedSalary + " рублей. Годовой доход вырос на " + kristinaIncomeDifference + " рублей.");
 
 
 
